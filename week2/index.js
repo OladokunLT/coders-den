c4cf0debf209f0ab3897dcdd09f73da4621450e6
Oladokun Lukman Tunde <lukmanoladokun86@gmail.com>
2022-10-08 18:50:26 +0100

    Done No 1,2 and 6.

---
+++ week2/index.js
@@ -0,0 +1,5 @@
+let challenge = "30 Days Of JavaScript";
+console.log(challenge);
+
+// Cut (slice) out the first word of the string using substr() or substring() method
+console.log(challenge.slice(0, 2));
