4c81db006621bcdd6d263c9bfcc721ded20cf300
Oladokun Lukman Tunde <lukmanoladokun86@gmail.com>
2022-10-11 18:05:39 +0100

    Solved up to num 8 challenge

--- week2/index.js
+++ week2/index.js
@@ -1,5 +1,25 @@
+//Level 1
+
+// 1. Declare a variable named challenge and assign it to an initial value '30 Days Of JavaScript'.
 let challenge = "30 Days Of JavaScript";
+
+// 2. Print the string on the browser console using console.log()
 console.log(challenge);
 
-// Cut (slice) out the first word of the string using substr() or substring() method
+// 3. Print the length of the string on the browser console using console.log()
+console.log(challenge.length);
+
+// 4. Change all the string characters to capital letters using toUpperCase() method
+console.log(challenge.toUpperCase());
+
+// 5. Change all the string characters to lowercase letters using toLowerCase() method
+console.log(challenge.toLowerCase())
+
+// 6. Cut (slice) out the first word of the string using substr() or substring() method
 console.log(challenge.slice(0, 2));
+
+// 7. Slice out the phrase Days Of JavaScript from 30 Days Of JavaScript.
+console.log(challenge.slice(3,))
+
+// 8. Check if the string contains a word Script using includes() method
+console.log(challenge.includes('Script'))
