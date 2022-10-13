8298a77f14790c659b4e22d339b16fe1597a5804
Oladokun Lukman Tunde <lukmanoladokun86@gmail.com>
2022-10-13 02:12:13 +0100

    Done except  few

--- week2/index.js
+++ week2/index.js
@@ -109,8 +109,58 @@ console.log(`jargon`.includes(`on`));
 console.log('I hope this course is not full of jargon'.match('jargon'));
 
 // 7. Generate a random number between 0 and 100 inclusively.
-console.log(Math.floor(Math.random() * 101)); //0 - 100
+console.log(Math.floor(Math.random() * 101)); 
 
-// 8. 
+// 8. Generate a random number between 50 and 100 inclusively.
 
 
+// 9. Generate a random number between 0 and 255 inclusively.
+
+
+// 10. Access the 'JavaScript' string characters using a random number.
+
+
+// 11. Use console.log() and escape characters to print the following pattern.
+
+console.log(
+    `
+    ${1} ${1**0} ${1**1} ${1**2} ${1**3} \t
+    ${2} ${2**0} ${2**1} ${2**2} ${2**3} \t
+    ${3} ${3**0} ${3**1} ${3**2} ${3**3} \t
+    ${4} ${4**0} ${4**1} ${4**2} ${4**3} \t
+    ${5} ${5**0} ${5**1} ${5**2} ${5**3} \t
+    `
+);
+
+// 12. Use substr to slice out the phrase because because because from the following sentence:'You cannot end a sentence with because because because is a conjunction'
+console.log(sentence); // You cannot end a ...
+let startIndex = sentence.indexOf('because');
+let stopIndex = 'because because because'.length + startIndex;
+console.log(sentence.slice(startIndex, stopIndex));
+
+// ---- LEVEL 3 --------------------------------------------
+
+/* 1. 'Love is the best thing in this world. 
+Some found their love and some are still looking for their love.' 
+Count the number of word love in this sentence.*/
+let quote = `Love is the best thing in this world. Some found their love and some are still looking for their love.`;
+console.log(quote.match(/love/gi).length);
+
+/* 2. Use match() to count the number of all because in the following sentence:
+'You cannot end a sentence with because because because is a conjunction'*/
+console.log(sentence.match(/because/gi).length);
+
+/* 3. Clean the following text and find the most frequent word 
+(hint, use replace and regular expressions).*/
+const sentence1 = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
+let clean = sentence1.replace(/[%$@&#;!]/g, "").split(" ");
+Math.max()
+console.log(clean)
+
+/* 4. Calculate the total annual income of the person 
+by extracting the numbers from the following text. 
+'He earns 5000 euro from salary per month, 10000 euro annual bonus, 
+15000 euro online courses per month.'*/
+const salaryStr = 'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.'
+let numArr = salaryStr.match(/\d+/g);
+console.log(+numArr[0] + +numArr[1] + +numArr[2])
