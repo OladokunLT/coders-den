4a193d60bad485ad4693e6519de8c8dfcf3afbd6
Oladokun Lukman Tunde <lukmanoladokun86@gmail.com>
2022-10-16 18:47:44 +0100

    Updated solution to penultimate question

--- week2/index.js
+++ week2/index.js
@@ -62,6 +62,7 @@ console.log(sentence.lastIndexOf("because"));
 /* 19. Use search to find the position of the first occurrence of the word because in the following sentence:
 'You cannot end a sentence with because because because is a conjunction' */
 console.log(sentence.search(/because/gi));
+console.log(sentence.search("because"));
 
 /* 20. Use trim() to remove any trailing whitespace at the 
 beginning and the end of a string.E.g ' 30 Days Of JavaScript '. */
@@ -74,7 +75,7 @@ console.log(challenge.startsWith('30 Days Of'));
 console.log(challenge.endsWith('JavaScript'));
 
 // 23. Use match() method to find all the a’s in 30 Days Of JavaScript
-console.log(challenge.match(/a/g));
+console.log(challenge.match(/a/gi)); // 
 
 // 24. Use concat() and merge '30 Days of' and 'JavaScript' to a single string, '30 Days Of JavaScript'
 console.log('30 Days of'.concat(" ", 'JavaScript'));
@@ -92,7 +93,7 @@ let motherTeresa = "Love is not patronizing and charity isn\'t about pity, it is
 console.log(motherTeresa);
 
 // 3. Check if typeof '10' is exactly equal to 10. If not make it exactly equal.
-console.log(typeof '10' === 10 );
+console.log(typeof '10' === 10 ); // false
 let numTen = parseInt('10');
 console.log(numTen === 10);
 
@@ -108,11 +109,12 @@ console.log(`jargon`.includes(`on`));
 // 6. I hope this course is not full of jargon. Check if jargon is in the sentence.
 console.log('I hope this course is not full of jargon'.match('jargon'));
 
-// 7. Generate a random number between 0 and 100 inclusively.
-console.log(Math.floor(Math.random() * (100 - 0 + 1)) + 0)  
+// 7. Generate a random number between 0 and 100 inclusively. 
+console.log(Math.floor(Math.random() * (100 - 0 + 1)) + 0 )  
 // 8. Generate a random number between 50 and 100 inclusively.
 let min=50, max= 100;
 console.log(Math.floor(Math.random() * (max - min + 1)) + min); 
+Math.random();
 
 // 9. Generate a random number between 0 and 255 inclusively.
 console.log(Math.floor(Math.random() * (225 - 0 + 1)) + 0)
@@ -152,9 +154,37 @@ console.log(sentence.match(/because/gi).length);
 
 /* 3. Clean the following text and find the most frequent word 
 (hint, use replace and regular expressions).*/
-const sentence1 = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
-let clean = sentence1.replace(/[%$@&#;!]/g, "").split(" ");
-console.log(clean)
+const sentence1 = `%I $am@% a %tea@cher%, &and& 
+I lo%#ve %te@a@ching%;. 
+The@re $is no@th@ing; 
+&as& mo@re rewarding as educa@ting 
+&and& @emp%o@weri@ng peo@ple. 
+;I found tea@ching m%o@re interesting 
+tha@n any ot#her %jo@bs. %Do@es thi%s 
+mo@tiv#ate yo@u to be a tea@cher!? 
+%Th#is 30#Days&OfJavaScript &is 
+al@so $the $resu@lt of &love& of tea&ching`
+let clean = sentence1.replace(/[%$@&#;!]/g, "");
+console.log(clean);
+console.log(clean.match(/I/g).length);
+console.log(clean.match(/am/g).length);
+console.log(clean.match(/a/g).length); 
+console.log(clean.match(/teacher/gi).length);
+console.log(clean.match(/and/gi).length);
+console.log(clean.match(/love/gi).length);
+console.log(clean.match(/teaching/gi).length)
+console.log(clean.match(/there/gi).length);
+console.log(clean.match(/is/gi).length);
+console.log(clean.match(/as/gi).length);
+console.log(clean.match(/more/gi).length);
+console.log(clean.match(/rewarding/gi).length);
+console.log(clean.match(/educating/gi).length);
+console.log(clean.match(/than/gi).length);
+console.log(clean.match(/any/gi).length);
+console.log(clean.match(/other/gi).length);
+console.log(clean.match(/jobs/gi).length);
+console.log(clean.match(/does/gi).length);
+console.log(clean.match(/this/gi).length);
 
 /* 4. Calculate the total annual income of the person 
 by extracting the numbers from the following text. 
